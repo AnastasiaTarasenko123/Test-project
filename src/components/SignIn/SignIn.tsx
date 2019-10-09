a17d17c7e4bf75e9efb8193dbd39b590d5333107
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2019-10-09 10:25:57 +0300

    complete-sign-in

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,25 +1,73 @@
 import React from 'react'
+import { compose } from 'recompose'
+import { withRouter } from 'react-router-dom';
+
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SignUpLink from '../SignUp/SignUpLink'
 import './SignIn.css'
+import Firebase from '../Firebase/Firebase'
+import * as ROUTES from '../../constants/routs';
+import { withFirebase } from '../Firebase/FirebaseContext'
 
-const SignInPage: React.FC = ():React.ReactElement =>(
-         <div className="sign-in-page">
-            <SignInForm/>
-            <br/>
-            <SignUpLink/>
-        </div>
+const SignInPage: React.FC = (): React.ReactElement => (
+    <div className="sign-in-page">
+        <SignInForm />
+        <br />
+        <SignUpLink />
+    </div>
 )
 
-class SignInForm extends React.Component {
+interface IState {
+    email: string,
+    password: string,
+    error: Error | null
+}
+
+interface IProps { 
+    firebase: Firebase | null,
+    history: any
+}
+
+class SignInFormBase extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = this.value;
+    }
+    value: IState = {
+        email: "",
+        password: "",
+        error: null
+    }
+    onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (this.props.firebase !== null) {
+            this.props.firebase.doSignInWithEmailAndPassword(this.state.email, this.state.password)
+                .then(authUser => {
+                    this.setState({ ... this.value });
+                    this.props.history.push(ROUTES.EDITOR);
+                })
+                .catch(error => {
+                    this.setState({ error });
+                });
+        }
+        event.preventDefault();
+    }
+    onChange = (key: keyof IState) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target
+        this.setState(prev => ({
+            ...prev,
+            [key]: value
+        }))
+    }
     render() {
+        const isInvalid = this.state.email === '' || this.state.password === '';
         return (
-            <form className="test-form" noValidate autoComplete="off">
+            <form className="test-form" noValidate autoComplete="off" onSubmit={this.onSubmit}>
                 <TextField
                     id="outlined-email-input"
                     label="Email"
-                    className="email-c"
+                    className="email-sign-in"
+                    onChange={this.onChange('email')}
                     type="email"
                     autoComplete="email"
                     margin="normal"
@@ -29,7 +77,8 @@ class SignInForm extends React.Component {
                 <TextField
                     id="outlined-password-input"
                     label="Password"
-                    className="password-c"
+                    className="password-sign-up"
+                    onChange={this.onChange('password')}
                     type="password"
                     autoComplete="current-password"
                     margin="normal"
@@ -37,12 +86,15 @@ class SignInForm extends React.Component {
                 />
                 <br />
                 <br />
-                <Button variant="contained" className='button-sign-in'>
+                <Button variant="contained" className='button-sign-in' type="submit" disabled={isInvalid}>
                     Login
                 </Button>
+                {this.state.error && <p>{this.state.error.message}</p>}
             </form>
         );
     }
 }
+
+const SignInForm = compose<IProps, {}>(withRouter, withFirebase)(SignInFormBase);
 
 export default SignInPage;
