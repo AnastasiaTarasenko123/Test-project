fe9f560f5aea7aadbaa32b0c4055d2a390ca4c89
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2019-10-10 11:33:15 +0300

    Session Handling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,17 +3,60 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 import SignUpPage from '../SignUp/SignUp';
 import SignInPage from '../SignIn/SignIn'
+//import Dashboard from '../Dashboard/Dashboard'
+//import Editor from '../Editor/Editor'
 import * as ROUTES from '../../constants/routs';
 import './App.css'
+import Firebase from '../Firebase/Firebase';
+import { withFirebase } from '../Firebase/FirebaseContext';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-    </div>
-  </Router>
-);
+interface IProps { 
+  firebase: Firebase
+}
 
-export default App;
+interface IState {
+  authUser: any
+}
+
+class App extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      authUser: null
+    }
+  }
+
+  listener: any;
+
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(
+      authUser => {
+        authUser 
+        ? this.setState({authUser}) 
+        : this.setState({authUser: null}) 
+      }
+    ); 
+  }
+
+  componentWillUnmount() {
+    this.listener();
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <Navigation authUser = {this.state.authUser}/>
+            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+            <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+            {/* <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+                <Route path={ROUTES.EDITOR} component={Editor} /> */}
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default withFirebase(App);
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routs'
-import "./Navigation.css"
 import SignOut from '../SignOut/SignOut';
 
-const Navigation = () => (
+import "./Navigation.css"
+
+interface IProps {
+  authUser: null
+}
+
+const Navigation: React.FC<IProps> = (props): React.ReactElement => (
+  <div>{props.authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth: React.FC = () => (
   <div>
     <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-      </li>
       <li>
         <Link to={ROUTES.DASHBOARD}>Dashboard</Link>
       </li>
@@ -20,9 +23,20 @@ const Navigation = () => (
         <Link to={ROUTES.EDITOR}>Editor</Link>
       </li>
       <li>
-        <SignOut/>
+        <SignOut />
       </li>
     </ul>
   </div>
 );
+
+const NavigationNonAuth: React.FC = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+    </ul>
+  </div>
+);
+
 export default Navigation;
