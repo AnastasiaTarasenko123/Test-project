2470ffe56a78b2adeb6a287e10b0fffa92a10913
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2019-10-25 09:29:02 +0300

    some-update-with-Map

--- src/components/MapContainer/MapContainer.tsx
+++ src/components/MapContainer/MapContainer.tsx
@@ -7,27 +7,49 @@ interface IProps extends MapProps {
 }
 
 interface IState {
+        selectedPlace: google.maps.LatLng,
+        activeMarker: Marker
+        //showingInfoWindthis.state = {ow: true
 }
 
 export class MapContainer extends React.Component<IProps, IState> {
+    tempLng = new google.maps.LatLng({lat: 37.778519, lng: -122.405640});
     constructor(props: IProps) {
         super(props);
+        this.state = {
+            selectedPlace: this.tempLng,
+            activeMarker: new Marker({
+                position: this.tempLng
+            })
+           // showingInfoWindow: activeMarker
+        }
     }
 
     onMarkerClick = (props: any, marker: any, e: any) => {
         this.setState({
-            selectedPlace: props,
-            activeMarker: marker,
-            showingInfoWindow: true
+          //  selectedPlace: props,
+          //  activeMarker: null,
+           // showingInfoWindow: true
+        });
+    }
+
+    mapClicked = (mapProps: any, map: any, clickEvent: any) => {
+        console.log(clickEvent.latLng);
+        this.setState({
+            selectedPlace: clickEvent.latLng,
+            activeMarker: new Marker ({position: clickEvent.latLng})
         });
     }
 
     render() {
+        const { selectedPlace } = this.state;
+        console.log(selectedPlace);
         return (
             <div className="mapGoogle">
-                {/* onClick={this.onMapClicked} */}
-                <Map google={this.props.google}>
-                    <Marker onClick={this.onMarkerClick}></Marker> 
+                <Map google={this.props.google}
+                    onClick={this.mapClicked}>
+                    <Marker onClick={this.onMarkerClick} 
+                    position={selectedPlace}></Marker>
                 </Map>
             </div>
         );
