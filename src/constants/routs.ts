0b1664c33c2bfa4613853f630dd58d54bfd4745e
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2019-11-12 17:35:48 +0200

    route

--- src/constants/routs.ts
+++ src/constants/routs.ts
@@ -1,6 +1,6 @@
 export const SIGN_UP = '/signup'
 export const SIGN_IN = '/signin'
-export const DASHBOARD = '/'
+export const DASHBOARD = '/dashboard'
 export const EDITOR = '/editor'
 export const APP_INFO = '/appinfo'
 export const LISTS = '/lists'
