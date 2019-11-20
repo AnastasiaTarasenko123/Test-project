4aec31bc573b4aeb9d72194ac5599d92c511972a
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2019-11-20 10:28:11 +0200

    routs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,6 +16,10 @@ import NeatlyMain from '../../components2/NeatlyMain'
 import SignUp2 from '../../components2/SignUp2'
 import SignIn2 from '../../components2/SignIn2'
 import ForgotPassword2 from '../../components2/ForgotPassword2'
+import { ResetPassword2 } from '../../components2/ResetPassword2'
+import { Congratulations } from '../../components2/Congratulations'
+import { ConfirmEmail } from '../../components2/ConfirmEmail'
+import { SignInCompany } from '../../components2/SignInCompany'
 
 interface IProps extends RouteComponentProps<RouteParams> { }
 
@@ -29,6 +33,10 @@ const App: React.FC<IProps> = (props: IProps) => (
     <Route path={ROUTES.NEATLY_SIGN_UP} component={SignUp2} />
     <Route path={ROUTES.NEATLY_SIGN_IN} component={SignIn2} />
     <Route path={ROUTES.NEATLY_FORGOT_PASSWORD} component={ForgotPassword2} />
+    <Route path={ROUTES.NEATLY_RESET_PASSWORD} component={ResetPassword2} />
+    <Route path={ROUTES.NEATLY_CONFIRM_EMAIL} component={ConfirmEmail} />
+    <Route path={ROUTES.NEATLY_CONGRATULATIONS} component={Congratulations} />
+    <Route path={ROUTES.NEATLY_SIGN_IN_COMPANY} component={SignInCompany} />
     <Route path={`${ROUTES.EDITOR}/:appId`} component={Editor} />
     <Route path={`${ROUTES.EDITOR}/:appId${ROUTES.APP_INFO}`} component={AppInfo} />
     <Route path={`${ROUTES.EDITOR}/:appId${ROUTES.LISTS}`} component={Lists} />
---
+++ src/components2/ResetPassword2/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ResetPassword2: React.FC = () => (
+  <div>
+
+  </div>
+)
