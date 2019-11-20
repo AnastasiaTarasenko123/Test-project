4283035ed06216e97dd3b779bd55f22aee8c96ef
AnastasiaTarasenko123 <anastasiyatarasenko2000@gmail.com>
2019-11-20 10:36:52 +0200

    reset-password

--- src/components2/ResetPassword2/index.tsx
+++ src/components2/ResetPassword2/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import NeatlyMain from '../NeatlyMain'
+import { TextField, Button } from '@material-ui/core'
 
-export const ResetPassword2: React.FC = () => (
-  <div>
+import './style.scss'
 
-  </div>
-)
+class ResetPassword2 extends React.Component {
+  render() {
+    return (
+      <NeatlyMain>
+        <div className="neatly-reset-password">
+          <h2>Reset Password</h2>
+          <p className="tooltip">Please create a new password for your account</p>
+          <TextField
+            className="input-reset-password"
+            label="New Password"
+            type="password"
+            margin="normal"
+          />
+          <TextField
+            className="input-reset-password"
+            label="Confirm Password"
+            type="password"
+            margin="normal"
+          />
+          <Button variant="contained" className="input-reset-password btn-neatly-blue">
+            Confrim
+        </Button>
+        </div>
+      </NeatlyMain>
+    )
+  }
+}
+
+export default ResetPassword2
